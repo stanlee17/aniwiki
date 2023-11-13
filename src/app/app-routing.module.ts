fa9cea0ea641e609455246cfea63c6fb20242490
Yves <stanz.ferigh@gmail.com>
2023-11-13 11:50:48 +1100

    router outlet implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './pages/bookmarks/bookmarks.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'bookmarks', component: BookmarksComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,25 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './pages/header/header.component';
 import { TopratedComponent } from './components/toprated/toprated.component';
 import { AiringComponent } from './components/airing/airing.component';
 import { UpcomingComponent } from './components/upcoming/upcoming.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { BookmarksComponent } from './pages/bookmarks/bookmarks.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TopratedComponent, AiringComponent, UpcomingComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TopratedComponent,
+    AiringComponent,
+    UpcomingComponent,
+    FooterComponent,
+    BookmarksComponent,
+    HomeComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
